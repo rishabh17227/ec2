cf4a76716e2d3f953f38cf0e90f9506cfcb2244e
rishabh17227 <89865136+rishabh17227@users.noreply.github.com>
2022-07-07 19:22:59 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region = "us-east-1"
-  # access_key="AKIARKOWWZD5YJB2MP5F"
-  # secret_key="zx1nuXffPbga2QGyzggw9IyxVMypJeqHvNy0bgZ"
+  access_key="AKIARKOWWZD57QUODYZM"
+  secret_key="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"
   }
 
